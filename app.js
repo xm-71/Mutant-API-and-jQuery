53b8b5424ad7e6823d3b2431c9b156ed0aeb488e
xm-71 <alexcannon2@live.com>
2016-06-07 21:11:05 -0400

    List working with added Nav bar & buttons

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ $(document).on('click', 'a.delete', function(ev) {
   deleteMutant(id, li);
 });
 
+$(document).on('click', 'a.edit', function(ev) {
+  var li = $(ev.currentTarget).closest('li');
+  var id = li.data('id');
+  var mutantName = li.find('.mutant-name').text();
+  var form = $('form#edit_mutant').get(0);
+  $(form.mutantName).val(mutantName);
+  $(form.mutantId).val(id);
+});
+
+$('form#new_mutant').on('submit', createMutant);
+$('form#edit_mutant').on('submit', updateMutant);
+
 $(function() {
   $.get({
     url: mutantsUrl,
